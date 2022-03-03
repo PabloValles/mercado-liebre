a9b0eadbf02c60166d4e4eee9b6402fdcfe44059
PabloValles <valles.pablo@hotmail.com>
2022-03-03 11:49:32 -0300

    Microdesafío paso 2: ok

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const app = express();
 
 app.listen(3030, () => console.log("Server runing in port 3030."));
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});

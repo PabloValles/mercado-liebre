72c042ad3b6a99fcf585f9a15c8eec34f318ee2c
PabloValles <valles.pablo@hotmail.com>
2022-03-17 15:27:28 -0300

    new: login y register

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ app.use(express.static(path.resolve(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
